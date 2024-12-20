233d08bc218a0f5b92f37dc212d40b16c7b49656
Lavanyacheshani <143724777+Lavanyacheshani@users.noreply.github.com>
2024-12-20 12:26:53 +0530

    header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,25 +1,25 @@
-import menu from "../../img/menu-svgrepo-com.svg"
-import cart from "../../img/cart-check-svgrepo-com.svg"
-import search from "../../img/search-svgrepo-com.svg"
-
+import menu from "../../img/menu-svgrepo-com.svg";
+import cart from "../../img/cart-check-svgrepo-com.svg";
+import search from "../../img/search-svgrepo-com.svg";
 
 const Header = () => {
-    return <header className="w-full p-5 ">
+    return (
+        <header className="fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow ">
             {/* Header left */}
-            <div>
-                <img src={menu}alt="menu" />
-                <h1> LavSan <span>Store</span>  </h1>
-                <div>
-                    <input type="text" placeholder="Search"/>
-                    <img src={search} alt="search" />
+            <div className="flex items-center ">
+                <img src={menu} alt="menu" className="w-6 h-6 object-contain cursor-pointer"/>
+                <h1 className=" ml-2 text-sm font-semibold text-orange-500 ">
+                    LavSan <span className="text-white">Store</span>
+                </h1>  
+                <div className=" overflow-hidden ml-2 flex items-center rounded-full bg-[#f1e6e6]">
+                    <input type="text" placeholder="Search" className=" ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit"/>
+                    <img src={search} alt="search" className="w-6 h-6 object-contain cursor-pointer mr-2"/>
                 </div>
             </div>
             {/* Header right */}
-            <img src={cart} alt="cart" />
-        </header>;
-    
+            <img src={cart} alt="cart" className="w-6 h-6 object-contain " />
+        </header>
+    );
 };
 
 export default Header;
-
-
