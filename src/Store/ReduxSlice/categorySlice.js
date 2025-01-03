c9922104da85a271b13ccb0d56e003fc77699aae
Lavanyacheshani <143724777+Lavanyacheshani@users.noreply.github.com>
2025-01-03 22:12:52 +0530

    store

---
+++ src/Store/ReduxSlice/categorySlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    {
+        id:"1",
+        title:'1'
+    },
+    {
+        id:"2",
+        title:'2'
+    },
+    {
+        id:"3",
+        title:'3'
+    },
+    {
+        id:"4",
+        title:'4'
+    }
+    
+]
+const categorySlice = createSlice({
+    name:'categorySlice',
+    initialState,
+    reducers:{}
+})
